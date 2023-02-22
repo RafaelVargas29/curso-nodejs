23b49d9f79e1ca48f1e30249eea844ab0fc77180
Rafael Vargas <rafaelrgsv@gmail.com>
2023-02-22 18:35:52 -0300

    Utilizando os métodos HTTP

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -4,8 +4,25 @@ const app = express(); // chamando a função do express
 
 // Recebe o path da rota desejada (rota padrão localhost:portaDefinida/rotaDefinida)
 // Recebe uma requisição e uma resposta (request e response)
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World Ignite"}); 
+app.get("/courses", (request, response) => {
+    return response.json([ "Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+    return response.json([ "Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+// /:id : significa que a rota espera o parâmetro id do curso que severá ser modificado
+app.put("/courses/:id", (request, response) => {
+    return response.json([ "Curso 6", "Curso 2", "Curso 3", "Curso 4"]); // Alterando curso 1 para curso 6
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json([ "Curso 6", "Curso 7", "Curso 3", "Curso 4"]);  // Alterando o curso 2 para curso 7
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json([ "Curso 6", "Curso 2", "Curso 4"]);
 });
 
 app.listen(3333); // starta a aplicação na porta definida (endereço padrão: localhost:portaDefinida)
