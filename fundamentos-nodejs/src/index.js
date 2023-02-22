6f53cd02a8d31bb77952bb0251a9b36b53645430
Rafael Vargas <rafaelrgsv@gmail.com>
2023-02-22 18:09:14 -0300

    Adicionando o nodemon na aplicação

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -5,7 +5,7 @@ const app = express(); // chamando a função do express
 // Recebe o path da rota desejada (rota padrão localhost:portaDefinida/rotaDefinida)
 // Recebe uma requisição e uma resposta (request e response)
 app.get("/", (request, response) => {
-    return response.json({message: "Hello World Ignite!"}); 
+    return response.json({message: "Hello World Ignite"}); 
 });
 
 app.listen(3333); // starta a aplicação na porta definida (endereço padrão: localhost:portaDefinida)
